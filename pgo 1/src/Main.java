83c2d0e35bc97054207a31a284549f8b45ac67f2
Urszula Taranek <s32108@pjwstk.edu.pl>
2024-03-06 13:44:38 +0100

    update

--- pgo 1/src/Main.java
+++ pgo 1/src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        if(input>0) {
+            System.out.println("The number is positive");
+        } else if (input<0){
+            System.out.println("The number is negative");
+        } else {
+            System.out.println("The value equals 0");
         }
     }
 }
